60bf5593afaf8a233342b87d3c90ffe83bf6ab16
Ulises  Sosa <54693547+usosa1@users.noreply.github.com>
2022-06-27 17:02:02 -0500

    Create full_python.py

---
+++ full_python.py
@@ -0,0 +1,27 @@
+# showing atom x coordinates
+visited = {}
+print("X_coordinates:")
+for MYline in open('1a3d.pdb'):
+    list = MYline.split()
+    id = list[0]
+    if id == 'ATOM':
+        x_pos_cordinate = list[6:7]
+        print(x_pos_cordinate)
+        
+# showing atom x coordinates
+print("Y_coordinates:")
+for MYline in open('1a3d.pdb'):
+    list = MYline.split()
+    id = list[0]
+    if id == 'ATOM':
+        y_pos_cordinate = list[7:8]
+        print(y_pos_cordinate)
+        
+# showing atom x coordinates
+print("Z_coordinates:")
+for MYline in open('1a3d.pdb'):
+    list = MYline.split()
+    id = list[0]
+    if id == 'ATOM':
+        z_pos_cordinate = list[8:9]
+        print(z_pos_cordinate)
